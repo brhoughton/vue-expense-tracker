33311c0a59d6443c3324bf1d1af0385fd86098b0
Breck Houghton <breck.houghton@gmail.com>
2024-03-05 16:51:29 -0800

    Updated transaction functionality and list of existing transactions

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,25 @@
   <div class="container">
     <Balance />
     <IncomeExpenses />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
 
-<script>
-  import Header from "./components/Header.vue";
-  import Balance from "./components/Balance.vue";
-  import IncomeExpenses from "./components/IncomeExpenses.vue";
-  import TransactionList from "./components/TransactionList.vue";
-  import AddTransaction from "./components/AddTransaction.vue";
+<script setup>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
 
-  export default {
-    components: {
-      Header,
-      Balance,
-      IncomeExpenses,
-      TransactionList,
-      AddTransaction,
-    },
-  };
+import { ref } from 'vue';
+
+const transactions = [
+  { id: 1, text: "Raley's", amount: -147.98 },
+  { id: 2, text: "Chevron", amount: -98.24 },
+  { id: 3, text: "Amazon", amount: -13.98 },
+  { id: 4, text: "ACH Deposit", amount: 4166.66 },
+  { id: 5, text: "Apple", amount: -9.99 },
+];
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,23 +1,22 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li v-for="transaction in transactions" :key="transaction.id">
-      Cash <span>-$400</span><button class="delete-btn">x</button>
+    <li
+        v-for="transaction in transactions"
+        :key="transaction.id"
+        :class="transaction.amount < 0 ? 'minus' : 'plus'">
+      {{ transaction.text }}<span>${{ transaction.amount }}</span><button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        transactions: [
-          { id: 1, text: "Raleys", amount: -147.98 },
-          { id: 2, text: "Chevron", amount: -98.24 },
-          { id: 3, text: "Amazon", amount: -13.98 },
-          { id: 4, text: "Apple", amount: -9.99 },
-        ],
-      };
-    },
-  };
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  }
+});
 </script>
